cf1b214dab0c7e44b3f61fe0aee92cee43c97199
Venkat <venkat.varra@gmail.com>
2020-07-07 12:08:06 +0530

    gestures added

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const menuBtn = document.querySelector(".menuBtn")
 const menu = document.querySelector(".menu")
+const center = document.querySelector(".center")
 const content = document.querySelector(".menu-content")
 const menuRow = document.querySelector(".menu-table tr")
 menuBtn.addEventListener("click", () => {
     menu.style.left = "0"
 })
 menu.addEventListener("click", (event) => {
-    menu.style.left = "-100%"
+    menu.style.left = "-99%"
 })
 content.addEventListener("click", (event) => {
     event.stopPropagation()
 })
 menuRow.addEventListener("click", (event) => {
-    menu.style.left = "-100%"
+    menu.style.left = "-99%"
 })
+// var myElement = document.getElementById('myPara');
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(menu);
+mc.on("swiperight", function (ev) {
+    console.log(ev)
+    menu.style.left = "0"
+});
+var mc = new Hammer(menu);
+mc.on("swipeleft", function (ev) {
+    console.log(ev)
+    menu.style.left = "-99%"
+});
